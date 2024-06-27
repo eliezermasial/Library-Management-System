8c102eefea4f353b8a20268d0275f537e53c2af4
eliezer tamba <eliezermasiala2000@gmail.com>
2024-06-27 05:26:58 +0200

    putting the user page online

--- index.php
+++ index.php
@@ -1,57 +1,9 @@
 <?php
 require_once("pages/LIvres.php");
 require_once("pages/author.php");
+require_once("pages/user.php");
 
 
-class user{
-    
-    public function __construct (public string $nameUser,public string $mailUser,public $listBooks)
-    {}
-
-    public function getNameuser(): string {
-        return $this->nameUser;
-    }
-
-    public function getMail():string {
-        return $this->mailUser;
-    }
-
-    public function setListbook($listBooks): void {
-        $this->listBooks = $listBooks;
-    }
-
-    public function getListbooks() {
-        return $this->listBooks;
-    }
-
-    public function displatDispo(){
-        echo ", "."mail de l'utilisateur: {$this->mailUser} ".", "."nom de l'utilisateur : {$this->nameUser}";
-    }
-}
-
-class checkBook {
-    private const BOOK_AVAILABLE = "DISPONIBLE";
-    private const  BOOK_BORROW = "EMPRUNTE";
-
-    public function __construct(public string $statusBook = self::BOOK_AVAILABLE){
-
-    }
-
-    public function setStatus(string $statusBook): void {
-        if(in_array($statusBook,[self::BOOK_AVAILABLE , self::BOOK_BORROW])){
-            trigger_error(
-                sprintf("ceci n'appartient pas au table",$statusBook, emplode(",",[self::BOOK_AVAILABLE , self::BOOK_BORROW])),
-                E_USER_ERRO
-            );
-        }
-        
-        $this->statusBook = $statusBook;
-    }
-
-    public function getStatus() {
-        echo ", "."status du livre : {$this->statusBook}";
-    }
-}
 
 
 $livre = new author ("ocean",2000,"tamba","congolaise");
