6cade5629a2a0f96b8a0643aa10ce285e476a875
eliezer tamba <eliezermasiala2000@gmail.com>
2024-06-27 05:17:43 +0200

    putting the author page online

--- index.php
+++ index.php
@@ -1,54 +1,7 @@
 <?php
 require_once("pages/LIvres.php");
+require_once("pages/author.php");
 
-class author extends livre {
-    public array $author = [];
-    
-    public function __construct(public string $title, public float $year,
-    public string $name, public string $biography) {}
-
-    public function getName(): string {
-        return $this->name;
-    }
-
-    public function getBiography(): string {
-        return $this->biography;
-    }
-
-    public function addAuthor($name,$biography): void {
-        $this->author[] = $name;
-        $this->author[] = $biography;
-
-        $name = array_filter($this->author);
-        $biography = array_filter($this->author);
-    }
-
-    public function setAuthor($author): void {
-        $this->author = $author;
-    }
-
-    public function getAuthor() {
-
-        $author=$this->author ;
-
-        return $author;
-    }
-
-    public function displayArrayAuthor(){
-        
-        foreach($this->getAuthor() as $author){
-            echo '<pre>';
-                var_dump($autheur);
-                
-            echo '</pre>';
-        }
-    }
-
-    public function displayBook(): void {
-        echo "titre : {$this->title},\n"."année de publication : {$this->year},\n"."autheur : {$this->name},\n"."biography : {$this->biography}";
-    }
-
-}
 
 class user{
     
