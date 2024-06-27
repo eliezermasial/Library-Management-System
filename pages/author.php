54b3a25072f849e8ff3f0cba8fc88d81349d770c
eliezer tamba <eliezermasiala2000@gmail.com>
2024-06-27 05:14:51 +0200

    creation of author class and these different functionalities

--- pages/author.php
+++ pages/author.php
@@ -1,34 +1,50 @@
 <?php
-/*require_once("Livres.php");
+require_once("Livres.php");
 
-class author {
-    public function __construct(public string $name, public string $Biography)
-    {
-        $this->book = []; 
-    }
+class author extends livre {
+    public array $author = [];
+    
+    public function __construct(public string $title, public float $year,
+    public string $name, public string $biography) {}
 
-    public function getname(): string {
+    public function getName(): string {
         return $this->name;
     }
 
     public function getBiography(): string {
-        return $this->Biography;
+        return $this->biography;
     }
 
-    public function addBook($book) : void {
+    public function addAuthor($name,$biography): void {
+        $this->author[] = $name;
+        $this->author[] = $biography;
 
-        $this->book[] = $book;
-        $book = array_filter($this->book);
+        $name = array_filter($this->author);
+        $biography = array_filter($this->author);
     }
 
-    public function getBook($book) {
-        foreach($this->book as $livre){
-            echo $livre;
-        }
+    public function setAuthor($author): void {
+        $this->author = $author;
+    }
+
+    public function getAuthor() {
+
+        $author=$this->author ;
+
+        return $author;
+    }
+
+    public function displayArrayAuthor(){
         
+        foreach($this->getAuthor() as $author){
+            echo $author;
+        }
     }
- 
-}*/
 
+    public function displayBook(): void {
+        echo "titre : {$this->title},\n"."année de publication : {$this->year},\n"."autheur : {$this->name},\n"."biography : {$this->biography}";
+    }
+
+}
 
 ?>
